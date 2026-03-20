83d0c61ecc7a45a50bdcc20dae20262460d1223e
Karpys <remi.vedrenne.pro@gmail.com>
2026-03-20 12:46:15 +0100

    [CODE] Add on select tween

---
+++ FrenzyPuzzle/Assets/Scripts/CursorFollower.cs
@@ -0,0 +1,15 @@
+﻿namespace PuzzleFrenzy.Scripts
+{
+    using UnityEngine;
+
+    public class CursorFollower : MonoBehaviour
+    {
+        [SerializeField] private Camera m_InputCamera = null;
+        private void Update()
+        {
+            Vector3 pos = m_InputCamera.ScreenToWorldPoint(Input.mousePosition);
+            pos.y = transform.position.y;
+            transform.position = pos;
+        }
+    }
+}
--- FrenzyPuzzle/Assets/Scripts/PuzzlePieceSelector.cs
+++ FrenzyPuzzle/Assets/Scripts/PuzzlePieceSelector.cs
@@ -1,5 +1,6 @@
 ﻿namespace PuzzleFrenzy.Scripts
 {
+    using System;
     using Helpers;
     using KarpysDev.KarpysUtils;
     using KarpysDev.KarpysUtils.TweenCustom;
@@ -7,10 +8,12 @@
 
     public class PuzzlePieceSelector : MonoBehaviour
     {
+        [SerializeField] private Transform m_CursorFollower = null;
         [SerializeField] private PuzzlePiecePlacer m_PuzzlePiecePlacer = null;
         [SerializeField] private Camera m_InputCamera = null;
         [SerializeField] private float m_RangeSelect = 1.0f;
-        
+
+        private Transform m_OldParent = null;
         private PuzzlePiece m_CurrentSelectedPiece = null;
         private PuzzlePiece[] m_Pieces = null;
 
@@ -27,16 +30,19 @@
 
             if (m_CurrentSelectedPiece != null)
             {
-                Vector3 inputPosition = m_InputCamera.ScreenToWorldPoint(Input.mousePosition);
-                inputPosition.y = 2;
-                m_CurrentSelectedPiece.transform.position = inputPosition;
+                // Vector3 inputPosition = m_InputCamera.ScreenToWorldPoint(Input.mousePosition);
+                // inputPosition.y = 2;
+                // m_CurrentSelectedPiece.transform.position = inputPosition;
             }
         }
 
         private void ReleasePiece()
         {
+            Debug.Log("Release Piece");
             if (m_CurrentSelectedPiece)
             {
+                m_CurrentSelectedPiece.transform.DoKill();
+                m_CurrentSelectedPiece.transform.parent = m_OldParent;
                 TryPlace(m_CurrentSelectedPiece);
             }
             m_CurrentSelectedPiece = null;
@@ -71,6 +77,9 @@
         {
             m_CurrentSelectedPiece = m_Pieces[id];
             m_CurrentSelectedPiece.transform.DoKill();
+            m_OldParent = m_CurrentSelectedPiece.transform.parent;
+            m_CurrentSelectedPiece.transform.parent = m_CursorFollower;
+            m_CurrentSelectedPiece.transform.DoLocalMove(new Vector3(0,-2,0), GlobalVariables.PuzzlePiecePlaceSpeed).SetEase(GlobalVariables.PuzzlePiecePlaceEase).SetMode(TweenMode.SPEED);
         }
     }
 }
